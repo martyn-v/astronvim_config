c25aed754e53b17d648cb361492bd9a1b7a6cf44
martyn-v <81797311+martyn-v@users.noreply.github.com>
2023-10-23 19:56:50 -0500

    Update community.lua
    Adds catppuccin

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,22 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        rainbow_delimiters = true,
+      },
+    },
+  },
 }
