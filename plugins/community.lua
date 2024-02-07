5800ca44d96ab7d4400b8d091e4dfbcd6977eadf
martyn-v <81797311+martyn-v@users.noreply.github.com>
2024-02-07 08:34:38 -0500

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,6 +7,7 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.pack.svelte"},
   { import = "astrocommunity.completion.copilot-lua-cmp"},
   { import = "astrocommunity.colorscheme.catppuccin" },
